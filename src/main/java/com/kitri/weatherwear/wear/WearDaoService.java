07027717791efcda17876ddf5da8fa161693054d
khjk <begywjd@naver.com>
2020-07-16 00:11:39 +0900

    Implement Message Repository,service, api

--- src/main/java/com/kitri/weatherwear/wear/WearDaoService.java
+++ src/main/java/com/kitri/weatherwear/wear/WearDaoService.java
@@ -6,9 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 
 @Slf4j
