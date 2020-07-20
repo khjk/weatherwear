83fca2f1f96035dace30415a46cd34befe544d2c
khjk <begywjd@naver.com>
2020-07-20 10:06:26 +0900

    reflect new api

--- src/main/resources/static/js/weather.js
+++ src/main/resources/static/js/weather.js
@@ -5,90 +5,71 @@ const summary_text = document.getElementById("summary");
 const cloudy_text = document.getElementById("clouds");
 const humidity_text = document.getElementById("humidity");
 const wind_text = document.getElementById("wind");
-
-const API_KEY = "f6c6e99c31msh914f92a9d0106afp15872ejsna05a25c5d6d0";
-const COORDS = 'coords';
-
-function getWeather(lat, lng){
-	console.log(lat, lng);
-	fetch("https://community-open-weather-map.p.rapidapi.com/weather?units=metric&lat=${lat}&lon={lng}&q=Seoul", {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-			"x-rapidapi-key": "bb23f20fbcmsh154b420950a6951p119310jsn6d089e641477"
-		}
-	})
-	.then(function(response){
-		return response.json();
-	}).then(function(json){
-		console.log(">>>>>weather.js json값" + JSON.stringify(json));
-		const temperature = json.main.temp; //28
-		const place = json.name; //Seoul
-		const cloudy = json.clouds.all;
-		const humidity = json.main.humidity;
-		const wind = json.wind.speed;
-		var summary = '';
-		if(cloudy > 80) {
-		    summary = 'wind';
-		}else if(wind > 5) {
-            summary = 'wind';
-		}else if(humidity>40 && temperature < 2) {
-	        summary = 'snow';
-	    }else if(humidity>80 && wind > 10) {
-	        summary = 'thunder';
-	    }else if(humidity > 80) {
-            summary = 'rain';
-        }else{
-   		    summary = 'sun';
-   		}
-		//localstorage
-		localStorage.setItem('temperature', json.main.temp); //나중에지우기
-		localStorage.setItem('place',json.name); //나중에 지우기
-		//view제어
-		weather_text.innerText = `${temperature}°C`;
-		location_text.innerText = `${place}`;
-		summary_text.innerHTML = `${summary}`;
-		cloudy_text.innerText = `${cloudy}%`;
-		humidity_text.innerText = `${humidity}%`;
-		wind_text.innerText = `${wind} m/s`;
-	})
-	.catch(err => {
-		console.log(err);
-	});
+const oneday_text = document.getElementById("oneDayAfter");
+const twoday_text = document.getElementById("twoDayAfter");
+const threeday_text = document.getElementById("threeDayAfter");
+const fourday_text = document.getElementById("fourDayAfter");
+const onedayIcon = document.getElementById("onedayIcon");
+const twodayIcon = document.getElementById("twodayIcon");
+const threedayIcon = document.getElementById("threedayIcon");
+const fourdayIcon = document.getElementById("fourdayIcon");
+function init(){
+    getYoilDateMonth();
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
 }
-
-function saveCoords(coordsObj){
-	localStorage.setItem(COORDS, JSON.stringify(coordsObj));
-}
-
 function handleGeoSuccess(position){
 	const latitude = position.coords.latitude;
-	const longitude = position.coords.longitude;
-	const coordsObj = {
-			latitude : latitude,
-			longitude : longitude
-	};
-	saveCoords(coordsObj);
-	getWeather(latitude, longitude);
+    const longitude = position.coords.longitude;
+    console.log(latitude, longitude);
+    var apiURI = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon="
+    + longitude + "&units=metric&lang=kr&appid=6e80afc16299c60c68363797958861a6";
+    $.ajax({
+        url: apiURI,
+        dataType: "json",
+        type: "GET",
+        async: "false",
+        success: function(response) {
+            console.log(response);
+            var current = response.current;
+            console.log("현재 온도" + current.temp);
+            console.log("현재 습도:"+ current.humidity +"%");
+            console.log("현재 구름:"+ current.clouds +"%");
+            console.log("현재 풍속:"+ current.wind_speed + "m/s");
+            console.log("현재 Summary:"+ current.weather[0].main);
+            var CurrentWeatherIcon = "http://openweathermap.org/img/wn/" + current.weather[0].icon + "@2x.png";
+            $("#currentWeatherIcon").attr("src",CurrentWeatherIcon);
+            weather_text.innerText = `${current.temp}°C`;
+            location_text.innerText = `${response.timezone}`;
+            summary_text.innerHTML = `${current.weather[0].main}`;
+		    cloudy_text.innerText = `${current.clouds}%`;
+		    humidity_text.innerText = `${current.humidity }%`;
+            wind_text.innerText = `${current.wind_speed} m/s`;
+
+            var oneDayAfter = response.daily[1];
+            var twoDayAfter = response.daily[2];
+            var threeDayAfter = response.daily[3];
+            var fourDayAfter = response.daily[4];
+            var fiveDayAfter = response.daily[5];
+            console.log("내일 온도:" + oneDayAfter.temp.day);
+            console.log("내일 습도:" + oneDayAfter.humidity);
+            console.log("내일 구름:" + oneDayAfter.clouds);
+            console.log("내일 풍속:" + oneDayAfter.wind_speed);
+            console.log("내일 강우량:" + oneDayAfter.rain);
+            console.log("내일 Summary:" + oneDayAfter.weather[0].main + "mm");
+            var OneDayWeatherIcon =  "http://openweathermap.org/img/wn/" + oneDayAfter.weather[0].icon + "@2x.png";
+            var twoDayWeatherIcon =  "http://openweathermap.org/img/wn/" + twoDayAfter.weather[0].icon + "@2x.png";
+            var threeDayWeatherIcon =  "http://openweathermap.org/img/wn/" + threeDayAfter.weather[0].icon + "@2x.png";
+            var fourDayWeatherIcon =  "http://openweathermap.org/img/wn/" + fourDayAfter.weather[0].icon + "@2x.png";
+            $("#onedayIcon").attr("src",OneDayWeatherIcon);
+            $("#twodayIcon").attr("src",twoDayWeatherIcon);
+            $("#threedayIcon").attr("src",threeDayWeatherIcon);
+            $("#fourdayIcon").attr("src",fourDayWeatherIcon);
+        }
+    })
 }
 
 function handleGeoError(){
 	console.log("Can't access geo location");
-}
-
-function askForCoords(){
-	navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
-}
-
-function loadCoords(){
-    const loadedCoords = localStorage.getItem(COORDS);
-    getYoilDateMonth();
-	if(loadedCoords === null){
-		askForCoords(); //좌표요청
-	}else{
-		const parsedCoords = JSON.parse(loadedCoords); //Javscript 객체로
-		getWeather(parsedCoords.latitude, parsedCoords.longitude);
-	}
 }
 
 function getYoilDateMonth(){
@@ -98,7 +79,15 @@ function getYoilDateMonth(){
     const day = date.getDay();
     var monthName = getMonthName(month);
     var dayName = getDayName(day);
+    var onedayName = getDayName(day+1);
+    var twodayName = getDayName(day+2);
+    var threedayName = getDayName(day+3);
+    var fourdayName = getDayName(day+4);
     date_text.innerHTML = `${dayName} ${monthName} ${date_num}`;
+    oneday_text.innerHTML = `${onedayName}`;
+    twoday_text.innerHTML = `${twodayName}`;
+    threeday_text.innerHTML = `${threedayName}`;
+    fourday_text.innerHTML = `${fourdayName}`;
 }
 
 function getMonthName(month) {
@@ -170,9 +159,4 @@ function getDayName(day) {
       return dayName;
 }
 
-function init(){
-	loadCoords();
-}
-
 init();
-
