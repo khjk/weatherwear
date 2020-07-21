e0199c382064ffe7a4bcd139f6d871255af4ecc2
jihyehyun <s____11@naver.com>
2020-07-21 13:56:10 +0900

    주석 수정

--- src/main/resources/static/js/save_wear.js
+++ src/main/resources/static/js/save_wear.js
@@ -1,12 +1,13 @@
 // 날짜 선택(달력) -- datepicker
 $(function() {
     var today = new Date();
-    // $('#date-piker-input').val($.datepicker.formatDate("yy-mm-dd", today));
+    $('#date-piker-input').val($.datepicker.formatDate("yy-mm-dd", today));
 
     $( "#date-piker-input" ).datepicker({
         dateFormat: "yy-mm-dd",
         duration: "fast",
-        // maxDate: today,
+        minDate: -5,
+        maxDate: today,
         beforeShowDay: DisableDates
     });
 } );
@@ -43,13 +44,12 @@ $(function () {
 //submit
 $(function () {
     $("#submit").click(function () {
+        var user_id = $("#user_id").val();
         var sendData = JSON.stringify(
             {
-                // *수정* session 처리
-                user_id: $("#user_id").val(),
-                // *수정* temp 코드 얻어오기
-                temp_code: 1,
-                // wear_date: $("#date-piker-input").val(),
+                // *수정 해야 함!!!!!* session 처리
+                user_id: user_id,
+                temp_code: getTempCode(user_id, ),
                 wear_date: getWearDate(),
                 wear_code: getWearCode()
             });
@@ -63,7 +63,6 @@ $(function () {
             success: function (data) {
                 confirm("등록 하시겠습니까?");
                 window.location.href="/";
-                //location.href = "../";
             }
         });
     });
@@ -126,10 +125,79 @@ function getWearDate(){
 
     for(i = 0; i<disableDoneDay.length; i++){
         if(disableDoneDay[i] == date){
-            alert("날짜를 다시 확인해주세요:)");
+            alert("이미 등록된 날짜입니다. 입력 날짜를 다시 확인해주세요:)");
             return false;
         }
     }
 
     return date;
 }
+
+function getTempCode(user_id) {
+
+    var date = getWearDate();
+    var latitude = 0;
+    var longitude = 0;
+
+    $.ajax({
+        url: "../api/v1/users/"+user_id,
+        async: false,
+        success: function(data) {
+            latitude = data.loc_latitude;
+            longitude = data.loc_longitude;
+        }
+    });
+
+    console.log(latitude);
+    console.log(longitude);
+
+    var regDate = new Date(date);
+    console.log("입력일: "+ regDate);
+
+    var uRegDate = Math.floor(+regDate/1000);
+    console.log("uRegDate: "+uRegDate);
+
+    var apiURI = "https://api.openweathermap.org/data/2.5/onecall/timemachine?"
+        + "lat=" + latitude
+        + "&lon=" + longitude
+        + "&dt="+ uRegDate
+        +"&appid=6e80afc16299c60c68363797958861a6";
+
+    var avg = 0;
+
+    $.ajax({
+        url: apiURI,
+        dataType: "json",
+        type: "GET",
+        async: false,
+        success: function(data) {
+            var sum = 0;
+            for(i=0; i<data.hourly.length; i++){
+                sum += data.hourly[i].temp - 273.15
+            }
+            avg = sum/data.hourly.length;
+            console.log("평균기온 : "+ avg);
+
+            }
+    });
+
+    var tempCode = searchTempCode(avg);
+
+    return tempCode;
+}
+
+function searchTempCode(temp) {
+    var tempCode = 0;
+    if(temp >= 28){ tempCode = 1; }
+    else if(temp<28 && temp>=23){ tempCode = 2; }
+    else if(temp<23 && temp>=20){ tempCode = 3; }
+    else if(temp<20 && temp>=17){ tempCode = 4;}
+    else if(temp<17 && temp>=12){ tempCode = 5;}
+    else if(temp<12 && temp>=9){ tempCode = 6;}
+    else if(temp<9 && temp>=5){ tempCode = 7;}
+    else if(temp<5){ tempCode = 8;}
+
+    return tempCode;
+}
+
+
