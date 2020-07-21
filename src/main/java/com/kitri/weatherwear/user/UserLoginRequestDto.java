091d5178ddedc388e90c3ab51d670dca9c0c4a1c
khjk <begywjd@naver.com>
2020-07-22 01:02:40 +0900

    awwwwwwwwwwwwwwwwwww

--- src/main/java/com/kitri/weatherwear/user/UserLoginRequestDto.java
+++ src/main/java/com/kitri/weatherwear/user/UserLoginRequestDto.java
@@ -9,11 +9,9 @@ import javax.validation.constraints.NotBlank;
 @Data
 @NoArgsConstructor
 public class UserLoginRequestDto {
-    @NotBlank(message = "이메일은 필수 입력값입니다.")
-    @Email(message = "이메일 형식에 맞지 않습니다.")
+
     private String id;
 
-    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
     private String password;
 
     @Builder
